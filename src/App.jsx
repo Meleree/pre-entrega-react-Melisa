77a12958d52d81d6cb1dcfdf4013974f077c8f0f
Meleree <melisa.gis@gmail.com>
2024-12-19 06:19:15 -0300

    al fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from "./components/ItemListContainer/NavBar/NavBar"
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
+import Footer from "./components/Footer/Footer"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import ContadorEventListener from "./components/ejemplo/ContadorEventListener"
 import Formulario from "./components/Form/Formulario"
@@ -20,7 +21,7 @@ function App() {
           <Route path="/ejemplos" element={<ContadorEventListener />} />
           <Route path="/formulario" element={<Formulario />} />
         </Routes>
-        <footer />
+        <Footer />
       </BrowserRouter>
     </div>
   )
